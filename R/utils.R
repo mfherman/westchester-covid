93be528ee9b89466b1346093581b4d4ce2abf12a
Matt Herman <mfherman@gmail.com>
2020-12-17 16:36:31 -0500

    update site with fresh data

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 build_site <- function() {
   
   # build site
-  rmarkdown::render_site(input = "site/")
+  rmarkdown::render_site(input = "site/", quiet = TRUE)
   
   # move site to docs/
   fs::dir_copy(path = "site/docs/", new_path = "docs/", overwrite = TRUE) 
