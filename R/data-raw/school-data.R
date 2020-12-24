696af7115dd001f9a4ca5451dd6158e667d4cfd6
Matt Herman <mfherman@gmail.com>
2020-12-24 18:15:21 -0500

    update site with fresh data

--- R/data-raw/school-data.R
+++ R/data-raw/school-data.R
@@ -1,34 +1,49 @@
 library(tidyverse)
 library(readxl)
 library(jsonlite)
+library(glue)
 
-https://schoolcovidreportcard.health.ny.gov/assets/district.neighbors.json
-https://schoolcovidreportcard.health.ny.gov/data/public/school.660101.660101030002.json
-
-
+# https://schoolcovidreportcard.health.ny.gov/assets/district.neighbors.json
+# https://schoolcovidreportcard.health.ny.gov/data/public/school.660101.660101030002.json
 
 school_dir <- fromJSON("https://schoolcovidreportcard.health.ny.gov/data/directory/public.directory.abbreviated.json") %>% 
   map_dfr(bind_rows, .id = "school")
 
+districts <- school_dir %>%
+  filter(type == "District") %>%
+  select(
+    district = school,
+    district_beds = districtBedsCode
+  )
 
-districts <- school_dir %>% 
-  filter(is.na(schoolBedsCode))
-
-
-library(glue)
-
-
-dist_code <- districts$districtBedsCode
-
-dist_json <- glue("https://schoolcovidreportcard.health.ny.gov/data/public/district.{dist_code}.json")
-
-
-map(dist_json[1], fromJSON)
+schools <- school_dir %>%
+  filter(type == "School") %>%
+  mutate(school = str_remove(school, " \\(.*")) %>% 
+  select(
+    school = school,
+    school_beds = schoolBedsCode,
+    district_beds = districtBedsCode,
+    address
+  )
 
 
 
 
-fromJSON("https://schoolcovidreportcard.health.ny.gov/data/public/district.660101.json")
+dist_json <- glue("https://schoolcovidreportcard.health.ny.gov/data/public/district.{districts$district_beds}.json")
+
+
+a <- map(dist_json[1], fromJSON)
+
+
+
+View(a)
+
+  as.data.frame()
+
+
+school <- fromJSON("https://schoolcovidreportcard.health.ny.gov/data/public/school.660101.660101030002.json")
+
+View(school)
 
 
 https://schoolcovidreportcard.health.ny.gov/data/public/district.660101.json
