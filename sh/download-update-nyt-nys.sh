b18bdea105924ad6ba82efdd328fde791c5d84fe
Matt Herman <mfherman@gmail.com>
2020-12-26 22:13:58 -0500

    update site with fresh data

--- sh/download-update-nyt-nys.sh
+++ sh/download-update-nyt-nys.sh
@@ -5,6 +5,7 @@ Rscript "R/pull/pull-nyt-cases-deaths.R"
 Rscript "R/pull/pull-hotspot-zones.R"
 Rscript "R/pull/pull-hospital-occupancy.R"
 Rscript "R/pull/pull-nursing-home-deaths.R"
+Rscript "R/pull/pull-school-cases.R"
 Rscript "R/build/build-site.R"
 
 git commit -am  "update site with fresh data"
