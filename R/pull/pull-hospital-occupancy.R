bde9c89d1198dcaa574d7cc9c6a59c7e5d87461c
Matt Herman <mherman@csg.org>
2021-08-25 14:21:45 -0400

    update municipality data

--- R/pull/pull-hospital-occupancy.R
+++ R/pull/pull-hospital-occupancy.R
@@ -1,7 +1,7 @@
 source(here::here("R/build/attach-packages.R"))
 message(glue("{Sys.time()} -- Starting download of HHS hospital data"))
 
-url <- "https://beta.healthdata.gov/api/views/anag-cw7u/rows.csv?accessType=DOWNLOAD"
+url <- "https://healthdata.gov/api/views/anag-cw7u/rows.csv?accessType=DOWNLOAD"
 
 hospital <- read_csv(url, col_types = cols(), guess_max = 1e6)
 
