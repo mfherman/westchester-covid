c752509f0700f80bb9123d432fe93389c62ac157
Matt Herman <mherman@csg.org>
2021-12-10 17:04:54 -0500

    update site with fresh data

--- R/pull/pull-school-cases.R
+++ R/pull/pull-school-cases.R
@@ -6,8 +6,8 @@ parse_school <- function(x) {
     school_beds = x$bedsCode,
     model = x$teachingModel,
     date = as.Date(lubridate::mdy_hms(x$updateDate)),
-    students = x$currentCounts$onSiteStudentPopulation,
-    staff = x$currentCounts$onSiteTeacherPopulation + x$currentCounts$onSiteStaffPopulation,
+    students = x$currentCounts$studentEnrolled,
+    staff = x$currentCounts$teacherEnrolled + x$currentCounts$staffEnrolled,
     all_cases_students = x$allTimeCounts$onSitePositiveStudents,
     all_cases_staff = x$allTimeCounts$onSitePositiveTeachers + x$allTimeCounts$onSitePositiveStaff,
     recent_cases_students = x$lastFourteenDaysCounts$onSitePositiveStudents,
@@ -24,3 +24,11 @@ write_csv(school_cases, "data/school-cases.csv")
 
 message(glue("Most recent data is from {max(school_cases$date)}"))
 message(glue("{Sys.time()} -- Finsished download of schools data"))
+
+
+
+parse_school(school_json[[2]])
+
+
+school_json[[68]]$allTimeCounts
+View(school_json[[2]])
