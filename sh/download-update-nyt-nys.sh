9a4bd8ab97d301347f3a6ae9f8d238118dd7efb3
Matt Herman <mherman@csg.org>
2022-10-05 15:45:22 -0400

    update municipality data

--- sh/download-update-nyt-nys.sh
+++ sh/download-update-nyt-nys.sh
@@ -3,7 +3,7 @@
 Rscript "R/pull/pull-nys-cases-tests.R"
 Rscript "R/pull/pull-nyt-cases-deaths.R"
 Rscript "R/pull/pull-hospital-occupancy.R"
-Rscript "R/pull/pull-school-cases.R"
+#Rscript "R/pull/pull-school-cases.R"
 #Rscript "R/pull/pull-cdc-vac.R"
 #Rscript "R/pull/pull-hotspot-zones.R"
 #Rscript "R/pull/pull-nursing-home-deaths.R"
